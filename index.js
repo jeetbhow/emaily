43791a430b2382f392ad553043e6bccbccd8246d
Jeet Bhowmik <jeetbhowm@gmail.com>
2025-02-01 02:35:44 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
